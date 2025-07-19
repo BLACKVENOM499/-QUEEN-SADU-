eb07033cd7ffd1359922ac45c312192ac0685329
BLACKVENOM499 <blackvenomx499@gmail.com>
2025-07-19 07:16:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'add your session',
+    SESSION_ID: process.env.SESSION_ID || 'Gd00DByC#Fz-5TI7PpzGELwat6_8AIJHn9NR4LuNeJkibiDsFpxQ',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     AUTO_VOICE:"true"
 };
